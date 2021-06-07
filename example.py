d814bda9365139e4740521f5d316ae52a9a75c5f
dominichsiao <72414659+dominichsiao@users.noreply.github.com>
2021-06-06 18:38:31 -0700

    added test for subtraction

--- example.py
+++ example.py
@@ -1,6 +1,2 @@
 def add(a, b):
     return a + b
-
-
-def subtract(a, b):
-    return a - b
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(example.add(1, 4), 5)
 
     def test_subtract_1(self):
-        self.assertEqual(example.subtract(2, 1), 1)
+        self.assertEqual(example.subtract(3, 1), 2)
 
 
 if __name__ == '__main__':
